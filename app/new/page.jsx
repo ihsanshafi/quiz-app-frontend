ccda4ae6370c10b3f32456a9416bd2521dbbcafb
AbassS101 <146258030+AbassS101@users.noreply.github.com>
2024-09-09 09:43:57 -0400

    Integrated my code with Ihsans and fixed his issue with the coomponents not loading

--- app/new/page.jsx
+++ app/new/page.jsx
@@ -1,9 +1,10 @@
 "use client";
 
-import Button from "@/components/btn";
-import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+
+import Navbar from "@/components/nav";
+import QuestionEntry from '@/components/dataEntry';
 
 const New = () => {
 
@@ -21,44 +22,12 @@ const New = () => {
 
   return (
     <div>
-        <nav className="bg-slate-00 p-4">
-        <div className="container mx-auto flex justify-between items-center">
-            <div className="text-white text-xl font-bold">
-            <Link href="/">Quizapp</Link>
-            </div>
-            
-            <div
-            className={`${
-                isOpen ? 'block' : 'hidden'
-            } w-full md:flex md:items-center md:w-auto`}
-            >
-            <ul className="text-white md:flex md:space-x-6 space-y-2 md:space-y-0">
-                
-                <li>
-                <Button onClick={handleClick}>Back</Button>
-                </li>
-            </ul>
-            </div>
+        <div>
+          <Navbar />
         </div>
-        </nav>
-
-        <main className="flex flex-col items-center justify-between p-24">
-            <QuestionEntry/>
-            <div className="p-4">
-      <label htmlFor="inputField" className="block text-lg font-semibold mb-2">
-        Enter question:
-      </label>
-      <input
-        type="text"
-        id="inputField"
-        className="p-2 border rounded w-full text-black"
-        placeholder="Type something..."
-      />
-      
-    </div>
-            <Button>Add</Button>
-        </main>
-        
+        <div>
+        <QuestionEntry />
+      </div>
     </div>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,11 @@
 
 import Navbar from '@/components/nav'
 
-import QuestionEntry from '@/components/dataEntry';
-
 export default function New() {
   return (
     <main>
       <div>
-        <QuestionEntry />
+        <Navbar />
       </div>
     </main>
   );
